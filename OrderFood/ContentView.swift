4b997a7c5c03b3df55c6858edf9117dcf0d03251
Alexandre Marques <a.carvalho-k2p@cogna.parceirosedu.com.br>
2024-09-03 20:12:16 -0300

    a test

---
+++ OrderFood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrderFood
+//
+//  Created by Alexandre Marques on 03/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, first APP")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrderFood/OrderFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderFoodApp.swift
+//  OrderFood
+//
+//  Created by Alexandre Marques on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct OrderFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
